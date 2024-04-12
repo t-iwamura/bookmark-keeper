1b3526e78319426d4077b125375911e85298b91f
Taiki Iwamura <takki.0206@gmail.com>
2024-04-12 16:15:32 +0900

    Implement login to Raindrop.io

--- bsafe/scripts/main.py
+++ bsafe/scripts/main.py
@@ -1,6 +1,12 @@
+import json
+from pathlib import Path
+
 import click
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+REPO_DIR_PATH = Path(__file__).parents[2].resolve()
 
 
 @click.command()
@@ -15,4 +21,17 @@ def main(gui) -> None:
         options.add_argument("--headless=new")
 
     browser = Chrome(options)
+    browser.implicitly_wait(2.0)
+
+    # Load credentials
+    json_path = REPO_DIR_PATH / "configs" / "credentials" / "raindrop_io.json"
+    with json_path.open("r") as f:
+        account_dict = json.load(f)
+
+    # Login to Raindrop.io
+    browser.get("https://app.raindrop.io")
+    browser.find_element("name", "email").send_keys(account_dict["email"])
+    browser.find_element("name", "password").send_keys(account_dict["password"])
+    browser.find_element(By.XPATH, "//input[@class='button-dQdc ']").click()
+
     browser.close()
