080e6199786ab43ea8dae49b691c6b843c0cdc83
Taiki Iwamura <takki.0206@gmail.com>
2024-04-12 15:02:25 +0900

    Launch Google Chrome by selenium

--- bsafe/scripts/main.py
+++ bsafe/scripts/main.py
@@ -1,7 +1,18 @@
 import click
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options
 
 
 @click.command()
-def main() -> None:
+@click.option(
+    "--gui/--no-gui", show_default=True, help="Whether to display a browser or not."
+)
+def main(gui) -> None:
     """CLI app to automatically backup Pocket items and Raindrop.io items"""
-    pass
+    # Configure browser
+    options = Options()
+    if not gui:
+        options.add_argument("--headless=new")
+
+    browser = Chrome(options)
+    browser.close()
